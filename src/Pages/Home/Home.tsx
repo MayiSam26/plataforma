6b7453f90776b6b8e3cf0bc00aab27fced7e0e90
MayiSam26 <73198951+MayiSam26@users.noreply.github.com>
2026-02-24 22:39:47 -0500

    actualizando

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -194,7 +194,7 @@ export default function Home() {
         </div>
         <div style={{ display: "flex", justifyContent: "start" }}>
           <Button
-            onClick={() => navigate("mascotas-perdidas")}
+            onClick={() => navigate("/colitas")}
             sx={{
               background: "#ED6436",
               py: 1,
--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,27 +1,26 @@
+import About from "../Pages/About";
+import Home from "../Pages/Home/Home";
 
-import About from "../Pages/About"
-import Home from "../Pages/Home/Home"
+type JXSComponent = () => JSX.Element;
 
-type JXSComponent =  () => JSX.Element
-interface navigate{
-    to:string,
-    path:string,
-    Component: JXSComponent,
-    name:string
+interface navigate {
+  to: string;
+  path: string;
+  Component: JXSComponent;
+  name: string;
 }
 
-export const routes : navigate[] = [
-    {
-        to:'home',
-        path:'/',
-        Component:Home,
-        name:'Home'
-    },
-    {
-        to:'about',
-        path:'/colitas-por-adoptar',
-        Component:About,
-        name:'Home'
-    }
-]
-
+export const routes: navigate[] = [
+  {
+    to: "home",
+    path: "/",
+    Component: Home,
+    name: "Home",
+  },
+  {
+    to: "colitas",
+    path: "/colitas",
+    Component: About, // si About es la página que lista todas las colitas, se queda
+    name: "Colitas",
+  },
+];
