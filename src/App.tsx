84b56160524f6a05eaaf3cb705066dfd2111e7f3
MayiSam26 <73198951+MayiSam26@users.noreply.github.com>
2026-02-24 22:57:56 -0500

    actualizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import logo from './logo.svg'; 
-import './App.css';
-import { RoutesApp } from './Routes/Route';
+import React, { useEffect } from "react";
+import "./App.css";
+import { RoutesApp } from "./Routes/Route";
 
 function App() {
-  return (
-    <RoutesApp />
-  );
+
+  useEffect(() => {
+    const originalTitle = "Refugio Colitas & Amor";
+
+    const titles = [
+      "No te vayas 😢",
+      "Las colitas te extrañan 🐶",
+      "Adopta una vida 💚",
+    ];
+
+    let index = 0;
+    let interval: any;
+
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        interval = setInterval(() => {
+          document.title = titles[index % titles.length];
+          index++;
+        }, 2000);
+      } else {
+        clearInterval(interval);
+        document.title = originalTitle;
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <RoutesApp />;
 }
 
 export default App;
