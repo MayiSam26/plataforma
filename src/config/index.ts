59dcbb178668d902958d072fcdb34a17d8183c72
MayiSam26 <73198951+MayiSam26@users.noreply.github.com>
2026-02-24 21:40:46 -0500

    actualizando

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 //https://dreambox.digital
 //http://localhost:4000
 export default {
-    pathBase: "https://bakendultimo-production.up.railway.app/"
+    pathBase: "https://bakendultimo-production.up.railway.app"
 };
 
