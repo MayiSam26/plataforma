a1966e22235dd8cb3176e0a3f17bfd993262ed0b
MayiSam26 <73198951+MayiSam26@users.noreply.github.com>
2026-03-03 23:38:22 -0500

    actualiza plis

--- src/Pages/Home/Component/Nav.tsx
+++ src/Pages/Home/Component/Nav.tsx
@@ -1,22 +1,31 @@
 import { Fragment } from "react/jsx-runtime";
-import {dataNav} from '../Interfaces/dataNav'
-export default function Nav(){
-    return(
-        <Fragment>
-             <div className="col-lg-8 text-center text-lg-right">
-                        <div className="d-inline-flex align-items-center">
-                            {
-                                dataNav.map((item:any) =>(
-                                    <div 
-                                     key={item.id}
-                                        className="d-inline-flex flex-column text-center px-3 border-right">
-                                        <h6>{item.name}</h6>
-                                        <p className="m-0">{item.description}</p>
-                                    </div>
-                                ))
-                            }
-                        </div>
+import { dataNav } from "../Interfaces/dataNav";
+
+export default function Nav() {
+  return (
+    <Fragment>
+      {/* contenedor correcto */}
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+
+          <div className="col-12 col-lg-8 text-center text-lg-right">
+            <div className="d-flex flex-wrap justify-content-center justify-content-lg-end">
+
+              {dataNav.map((item: any) => (
+                <div
+                  key={item.id}
+                  className="d-flex flex-column text-center px-3 border-right mb-2"
+                >
+                  <h6>{item.name}</h6>
+                  <p className="m-0">{item.description}</p>
                 </div>
-        </Fragment>
-    )
+              ))}
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </Fragment>
+  );
 }
