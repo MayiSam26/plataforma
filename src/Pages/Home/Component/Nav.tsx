f689ea131c7331bb94734240ae7851bc3e05bb7f
MayiSam26 <73198951+MayiSam26@users.noreply.github.com>
2026-03-04 21:17:09 -0500

    actu

--- src/Pages/Home/Component/Nav.tsx
+++ src/Pages/Home/Component/Nav.tsx
@@ -4,14 +4,12 @@ import { dataNav } from "../Interfaces/dataNav";
 export default function Nav() {
   return (
     <Fragment>
-      {/* ✅ Contenedor: en desktop a la derecha, en móvil centrado */}
       <div className="col-lg-8 d-flex justify-content-center justify-content-lg-end">
-        {/* ✅ Wrapper: permite que en pantallas medianas envuelva sin romper */}
         <div className="top-info d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">
           {dataNav.map((item: any, index: number) => (
             <div
               key={item.id}
-              className={`top-info__item text-center px-3 ${
+              className={`top-info__item text-center ${
                 index !== dataNav.length - 1 ? "top-info__divider" : ""
               }`}
             >
